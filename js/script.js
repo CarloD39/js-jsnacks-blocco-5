a8e68459c8455e5eedc2d944bdee7c76c2f3cccb
CarloD39 <carlodurand@hotmail.it>
2021-06-17 08:41:54 +0200

    snacks blocco 2

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// <!-- Creare un array di 3 studenti (3 oggetti in un array, quindi) con queste proprietà:
+// nome
+// cognome
+// age
+// descrizione
+// Per nome, cognome ed age assegnate direttamente nel codice i valori che volete.
+// ATTENZIONE: la descrizione invece sarà l’unico valore vuoto, stringa vuota.
+
+// Successivamente, con dei prompt chiediamo di volta in volta all’utente la descrizione di ciascun studente, salviamola quindi all’interno della sua proprietà.
+// Nota bene: questo naturalmente, in un ciclo, per ciascun studente. ;)
+// Stampiamo con un console.log il risultato di questo array di studenti aggiornato. -->
+
+
+var students = [
+
+    {
+        names: 'Pino',
+        Lastnames: 'Nopi',
+        age: 32,
+        description:''
+    },
+
+    {
+        names: 'Patroclo',
+        Lastnames: 'Bruno',
+        age: 22,
+        description:''
+    },
+
+    {
+        names: 'Ciccio',
+        Lastnames: 'Magro',
+        age: 50,
+        description:''
+    }
+]
+
+
+for(x = 0; x < students.length; x++){
+
+    students.description = prompt('vai scrivi');
+
+
+    // for(var k in students[0]) {
+        
+    // }
+
+}
+
+console.log(students);
+
+
+
+
+
+
+
